39950861bddeab21d8eddcee21771622e4f12350
Akshay Pawar <60501569+thehealer15@users.noreply.github.com>
2021-03-12 14:47:07 +0530

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,35 +1,36 @@
 import tkinter as tk 
 
 def createFile():
-    with open("Data.txt",'w') as f:
-        f.write("Name,Age\n")
+    with open("Contacts.txt",'w') as f:
+        f.write("Name,Contact Number\n")
 
 def update_file(a,b):
-    with open("Data.txt",'a') as f:
+    with open("Contacts.txt",'a') as f:
         f.write(f"{a},{b}\n")
+    tk.Label(root,text="Saved Successfully").pack(side='bottom')
 
 def fun():
     a= name_of_person.get()
-    b=education.get()
+    b=num.get()
     update_file(a,b)
     name_of_person.set("")
-    education.set("")
+    num.set("")
     
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Taking Input's")
+    root.title("Contacts")
     root.geometry("650x400")
     createFile()
     name_of_person = tk.StringVar()
-    education= tk.StringVar()
+    num= tk.StringVar()
 
-    # creating widget of userID and password
+    # creating widget of Name and contact Number
     tk.Label(root,text="Enter Name\n").pack(side='top')
-    ID=tk.Entry(root,textvariable=name_of_person)
-    ID.pack(side='top')
-    tk.Label(root,text="Enter Age\n").pack(side='top')
-    educa_wid =  tk.Entry(root,textvariable=education,type=number)
-    educa_wid.pack(side='top');
+    name_wid=tk.Entry(root,textvariable=name_of_person)
+    name_wid.pack(side='top')
+    tk.Label(root,text="Enter Number\n").pack(side='top')
+    num_wid =  tk.Entry(root,textvariable=num)
+    num_wid.pack(side='top');
 
     tk.Button(root,text='submit',command=fun).pack(side='top')
 
